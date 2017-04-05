ef00feee1cc26c201f31d70973ab7fc6e165f520
Furkan Usta <furkanusta17@gmail.com>
2017-04-05 19:40:34 +0300

    Search Page now works thourgh tags or and usernames

---
+++ app/src/main/java/com/tattood/tattod/TagItemAdapter.java
@@ -0,0 +1,8 @@
+package com.tattood.tattod;
+
+/**
+ * Created by eksi on 05/04/17.
+ */
+
+public class TagItemAdapter {
+}
---
+++ app/src/main/java/com/tattood/tattod/TattooEditActivity.java
@@ -0,0 +1,13 @@
+package com.tattood.tattod;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TattooEditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tattoo_edit);
+    }
+}
