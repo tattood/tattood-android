f813f55e419543c0a4046f14df658de6041858e1
berilbasakt <berilbasakt@gmail.com>
2017-04-09 16:23:06 +0300

    Kudan AR jar & AR Camera Activity

--- app/src/main/java/com/tattood/tattood/MainActivity.java
+++ app/src/main/java/com/tattood/tattood/MainActivity.java
@@ -1,5 +1,6 @@
 package com.tattood.tattood;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
@@ -10,6 +11,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import eu.kudan.kudan.ARAPIKey;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -42,6 +46,8 @@ public class MainActivity extends AppCompatActivity {
 //        });
         token = getIntent().getStringExtra("token");
         username = getIntent().getStringExtra("username");
+        ARAPIKey key = ARAPIKey.getInstance();
+        key.setAPIKey("sVmoznmKZ+4nFEHD6HoslwpC26PNuBZGHrikUwyon2BKSvza1yu2CqbSrae+pHPr1NHjhsf5pHQOZn8IEqXlqXFodGsrOJhxJANbMOdvnRLUi9/QWGqyRL9FViDmyohw6e5R7U4Ex8H7d7spLLvhfp5HFv56DgLr8c8sC2ipDtv9g1IjOTaY7UGxata3eulG2A/UkIdRv2NcotZXqan01xQUWFAislEwlGguParEYiwu11T4mqtU3dQBbfxpvxbczjdYz493YG3rAO2RHgT+5M5TJShJsz2irkNo71JD2Fzqf4AR2b4+7t1c55zKjegXzGS6Xa/rpNn9yiXUn7rUYIHNvN3cEQa9HsZiVxAV4vJgxFS+T/AxfWqKrEg1uj6xF5MsodZ2EkZ8mqliYIsxZqnFz+Re2HeWG8wvrEob0ZwRIO0TxppAemZc3HChTAPLcNt5gzeBk0oRP4wnrFAFFBDi8XjDocwTSVw++hWZb1qNHzt6bKLsMDRT057UVuuZB6M8f7EOQD79Oah0Vrx/3DUK6e9BEV8oGFNHtk1wyYEkg0i6RLhVSokGx//Qj36A4gCz3h1OjtfB0OuukbNq7xI1L/FcNQLmGYNGZwszARjGr9ESw1gVAkbQMxaV27uo/KoIq4+nR7RL8iT7t7NAaXCFIi24RR+7WGjTvKqWYjA=");
     }
 
 
@@ -96,6 +102,11 @@ public class MainActivity extends AppCompatActivity {
                     return "Profile";
             }
             return null;
+        }
+        public void openARCamera(View view)
+        {
+            Intent intent = new Intent(MainActivity.this, ARCameraViewActivity.class);
+            startActivity(intent);
         }
     }
 }
