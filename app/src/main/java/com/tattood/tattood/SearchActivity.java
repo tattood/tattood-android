b8d48fca5767e9c48f91e86f828d1c7d4735aeac
nazliozge <nazli.ucan@gmail.com>
2017-04-30 13:41:28 +0300

    Deleter user search from search page

--- app/src/main/java/com/tattood/tattood/SearchActivity.java
+++ app/src/main/java/com/tattood/tattood/SearchActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.provider.SearchRecentSuggestions;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
@@ -29,16 +30,8 @@ public class SearchActivity extends AppCompatActivity implements View.OnClickLis
         query = extras.getString("query");
 
         final RecyclerView tag_list = (RecyclerView) findViewById(R.id.tag_list);
-        tag_list.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+        tag_list.setLayoutManager(new GridLayoutManager(this, 3));
         tag_list.setAdapter(new TattooRecyclerViewAdapter(this));
-        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(this, LinearLayoutManager.HORIZONTAL);
-        tag_list.addItemDecoration(dividerItemDecoration );
-
-        final RecyclerView user_list = (RecyclerView) findViewById(R.id.user_list);
-        user_list.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
-        user_list.setAdapter(new UserRecyclerViewAdapter(this));
-        DividerItemDecoration divider_user = new DividerItemDecoration(this, LinearLayoutManager.VERTICAL);
-        tag_list.addItemDecoration(divider_user);
 
         Server.search(SearchActivity.this, query,
                 new Response.Listener<JSONObject>() {
@@ -46,24 +39,8 @@ public class SearchActivity extends AppCompatActivity implements View.OnClickLis
                     public void onResponse(JSONObject response) {
                         try {
                             JSONObject tags = response.getJSONObject("tags");
-                            JSONObject users = response.getJSONObject("users");
                             boolean not_found = false;
-                            if (!empty(users)) {
-                                TextView tv = (TextView) findViewById(R.id.text_user);
-                                tv.setVisibility(View.VISIBLE);
-                                Button button = (Button) findViewById(R.id.see_more_user);
-                                button.setVisibility(View.VISIBLE);
-                                user_list.setVisibility(View.VISIBLE);
-                                ((UserRecyclerViewAdapter) user_list.getAdapter()).set_data(users);
-                            }
-                            else {
-                                not_found = true;
-                            }
                             if (!empty(tags)) {
-                                TextView tv = (TextView) findViewById(R.id.text_tag);
-                                tv.setVisibility(View.VISIBLE);
-                                Button button = (Button) findViewById(R.id.see_more_tag);
-                                button.setVisibility(View.VISIBLE);
                                 tag_list.setVisibility(View.VISIBLE);
                                 ((TattooRecyclerViewAdapter) tag_list.getAdapter()).set_data(tags);
                             }
@@ -75,11 +52,6 @@ public class SearchActivity extends AppCompatActivity implements View.OnClickLis
                         }
                     }
                 });
-        Button see_more = (Button) findViewById(R.id.see_more_tag);
-        see_more.setOnClickListener(this);
-        see_more = (Button) findViewById(R.id.see_more_user);
-        see_more.setVisibility(View.INVISIBLE);
-//        see_more.setOnClickListener(this);
         handleIntent(getIntent());
     }
 
@@ -106,15 +78,16 @@ public class SearchActivity extends AppCompatActivity implements View.OnClickLis
         }
     }
 
+
     @Override
     public void onClick(View view) {
         Intent myIntent = new Intent(this, SeeMore.class);
         myIntent.putExtra("query", query);
-        if (view.getId() == R.id.see_more_tag) {
+        /*if (view.getId() == R.id.see_more_tag) {
             myIntent.putExtra("TAG", "TAG");
         } else if (view.getId() == R.id.see_more_user) {
             myIntent.putExtra("TAG", "USER");
-        }
+        }*/
         startActivity(myIntent);
     }
 }
